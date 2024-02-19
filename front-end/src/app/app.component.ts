c955014646ca06b0ed7074fd89a1f61e6bb1e268
Wesley Emanuel <wesleyemanuel.1517@gmail.com>
2024-02-18 23:03:22 -0300

    feat: create nav-toolbar

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavToolbarComponent } from './components/nav-toolbar/nav-toolbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavToolbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ProdutosComponent } from './pages/produtos/produtos.component';
+import { ClientesComponent } from './pages/clientes/clientes.component';
+import { PedidosComponent } from './pages/pedidos/pedidos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: ProdutosComponent },
+  { path: 'clientes', component: ClientesComponent },
+  { path: 'pedidos', component: PedidosComponent },
+];
